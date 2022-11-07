87085a483cb6bde3653e3efd564a8724613d857d
reza ghasemzadeh <rezaghasemzadeh1@gmail.com>
2022-11-07 23:49:18 +0330

    add reports migration and contributing

---
+++ migrations/2021_07_11_000000_create_reports_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateLoveLikesTable.
+ */
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->morphs('reportable');
+            $table->string('type')->nullable();
+            $table->timestamps();
+            $table->unique([
+                'reportable_type',
+                'reportable_id',
+                'user_id',
+            ], 'report_user_unique');
+            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
